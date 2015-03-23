5f5916ac6efac10b5e0009316d0f42951ceb3e40
yanqirenshi <yanqirenshi@gmail.com>
2015-03-23 09:17:26 +0900

    close #46

--- src/vertex.lisp
+++ src/vertex.lisp
@@ -23,12 +23,26 @@
   (not (null (get-object-with-id graph (class-name (class-of vertex)) (id vertex)))))
 
 
+(defun existp-relationship (graph shin &optional (ra-list nil))
+  (when (null ra-list)
+    (setf ra-list (get-ra-classes graph)))
+  (labels ((core (graph shin ra-list)
+             (when ra-list
+               (let ((ra (car ra-list))
+                     (id (id shin)))
+                 (if (or (gethash id (get-root-object graph (up::get-objects-slot-index-name ra 'from-id)))
+                         (gethash id (get-root-object graph (up::get-objects-slot-index-name ra 'to-id))))
+                     t
+                     (core graph shin (cdr ra-list)))))))
+    (core graph shin ra-list)))
+
 ;;;;;
 ;;;;; 2. 作成
 ;;;;;
 (defmethod tx-make-vertex ((graph banshou) (class-symbol symbol) &optional slots-and-values)
   (unless (vertexp class-symbol)
     (error* :bad-class 'shin class-symbol))
+  (add-shin-class graph class-symbol)
   (tx-create-object graph class-symbol slots-and-values))
 
 (defmethod make-vertex ((graph banshou) (class-symbol symbol) &optional slots-and-values)
@@ -42,10 +56,8 @@
 ;;;;;
 (defmethod tx-delete-vertex ((graph banshou) (vertex shin))
   ;; 現在関係があるものは削除できないようにしています。
-  (let ((vertex-class (class-name (class-of vertex)))
-        (edge-class 'ra))
-    (when (or (find-r-edge graph edge-class :from vertex)
-              (find-r-edge graph edge-class :to   vertex))
+  (let ((vertex-class (class-name (class-of vertex))))
+    (when (existp-relationship graph vertex)
       (error* :delete-failed-have-some-edge vertex-class))
     (execute-transaction
      (tx-delete-object graph vertex-class (id vertex)))))
@@ -61,5 +73,4 @@
 #|
 This file is a part of shinrabanshou project.
 Copyright (c) 2014 Satoshi Iwasaki (yanqirenshi@gmail.com)
-
 |#
